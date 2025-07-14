510124457e84a8bad954f79356397d7316992e4d
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-07-14 14:15:26 +0900

    HomePageにtableを追加

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,10 +2,30 @@ import NavigationButtons from "./NavigationButtons";
 
 const HomePage = () => {
   return (
-    <>
-      <div>HomePage</div>
-      <NavigationPanel />
-    </>
+    <div className="home-page-content">
+      <table>
+        <thead>
+          <tr>
+            <td>種類</td>
+            <td>中身</td>
+            <td>サイズ</td>
+            <td>価格</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>あ</td>
+            <td>あ</td>
+            <td>あ</td>
+            <td>あ</td>
+          </tr>
+        </tbody>
+      </table>
+      <div className="navigation-panel">
+        <NavigationButtons />
+        <p>使ったお金:</p>
+      </div>
+    </div>
   );
 };
 
