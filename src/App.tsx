d0137daa15742da07e0a5341f86cd15a5bf1307b
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-07-14 11:20:47 +0900

    EditFormのルートを設定

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import HomePage from "./components/HomePage";
 import PurchaseForm from "./components/PurchaseForm";
+import EditForm from "./components/EditForm";
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/purchaseForm" element={<PurchaseForm />}></Route>
+        <Route path="/editForm" element={<EditForm />}></Route>
       </Routes>
     </Router>
   );
