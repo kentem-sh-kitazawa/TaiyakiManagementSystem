66f7795b735ca2e9a7218435c994895938f0253d
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-07-14 11:20:09 +0900

    purchaseFormのルートを設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import HomePage from "./components/HomePage";
+import PurchaseForm from "./components/PurchaseForm";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
+        <Route path="/purchaseForm" element={<PurchaseForm />}></Route>
       </Routes>
     </Router>
   );
