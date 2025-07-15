8f84ca01d760b3cd3b2aec81f0343c291dc3d903
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-07-15 12:54:44 +0900

    PurchaseFormにスタイルを適用

--- src/components/PurchaseForm.tsx
+++ src/components/PurchaseForm.tsx
@@ -1,5 +1,6 @@
   return <div>PurchaseForm</div>;
 import type { taiyaki } from "../Types/TaiyakiType";
+import "../Styles/PurchaseForm.css";
 type Props = {
   taiyakiInfos: taiyaki[];
   taiyakiSizes: string[];
